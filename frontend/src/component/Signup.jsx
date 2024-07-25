5902889ad1fcf36271705bde5cf0276a22fe2c0d
alirazabilal <129405525+alirazabilal@users.noreply.github.com>
2024-07-25 13:54:04 +0300

    Update Signup.jsx

--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -14,7 +14,7 @@ const Signup = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { name, email, password, contact } = credentials;
-    const response = await fetch("http://localhost:5000/api/auth/signup", {
+    const response = await fetch("https://flightsystemvite-backend.onrender.com/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
